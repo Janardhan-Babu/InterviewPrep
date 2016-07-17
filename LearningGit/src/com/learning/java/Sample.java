d4df2909fd709153015e1fa0ba178eaaed0b30e6
RUSTY <RUSTY@janardhan>
2016-07-17 16:52:20 +0530

    Sample

---
+++ LearningGit/src/com/learning/java/Sample.java
@@ -0,0 +1,30 @@
+package com.learning.java;
+import java.util.*;
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		
+		int [] perm = new int[n];
+		for(int i =0;i<n;i++){
+			perm[i]=i;
+		}
+		
+		for (int i=0;i<m;i++){
+			int r =i+(int)(Math.random()*(n-i));
+			int t = perm[r];
+			perm[r]=perm[i];
+			perm[i]=t;
+		}
+		
+		for(int i=0; i < m; i++ ){
+			System.out.print(perm[i]+" ");
+		}
+			System.out.println();
+		
+	}
+
+}
